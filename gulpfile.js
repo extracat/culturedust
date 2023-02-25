a58afdcd97b492d91c36ae863a2ac27f6c223643
Tony <extracat@users.noreply.github.com>
2023-02-25 23:13:27 +0900

    save

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,37 @@
-const { src, dest, series, watch } = import('gulp')
-const del = import('del')
-const njk = import('gulp-nunjucks-render')
-const beautify = import('gulp-beautify')
+const { src, dest, series, watch } = require('gulp')
+const del = require('del')
+const njk = require('gulp-nunjucks-render')
+const beautify = require('gulp-beautify')
 
 function clean() {
-    return del(['dist'])
+    return del(['build'])
 }
 
 function html() {
-    return src('src/html/*.+(html|njk|shtml)')
+    return src('src/html/**/*.+(html|njk|shtml)')
         .pipe(
             njk({
                 path: ['src/html'],
             })
         )
         .pipe(beautify.html({ indent_size: 4, preserve_newlines: false }))
-        .pipe(dest('dist'))
+        .pipe(dest('build'))
 }
+
+function files() {
+  return src('src/html/**/*.+(jpg|png|gif|txt|rtf)')
+      .pipe(dest('build'))
+}
+
+function css() {
+  return src('src/css/**/*.+(css)')
+      .pipe(dest('build'))
+}
+
 
 function watchFiles() {
     watch('src/html/**/*', html)
 }
 
-exports.build = series(clean, html)
-exports.default = series(clean, html, watchFiles)
+exports.build = series(clean, files, css, html)
+exports.default = series(clean, files, css, html, watchFiles)
